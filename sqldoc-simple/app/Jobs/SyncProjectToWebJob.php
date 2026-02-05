bb3416300e367c6d6046311780f14d6cd698eeed
TAUBIRA Mehdi <mtaubira@iceb.com>
2026-02-05 12:04:04 +0100

    modif batch procedure 50

--- sqldoc-simple/app/Jobs/SyncProjectToWebJob.php
+++ sqldoc-simple/app/Jobs/SyncProjectToWebJob.php
@@ -29,7 +29,7 @@ class SyncProjectToWebJob implements ShouldQueue
     protected const BATCH_SIZE_FUNCTIONS = 50;
     protected const BATCH_SIZE_FUNC_INFO = 50;
     protected const BATCH_SIZE_FUNC_PARAMS = 100;
-    protected const BATCH_SIZE_PROCEDURES = 100;
+    protected const BATCH_SIZE_PROCEDURES = 50;
     protected const BATCH_SIZE_PS_INFO = 50;
     protected const BATCH_SIZE_PS_PARAMS = 50;
     protected const BATCH_SIZE_TRIGGERS = 50;
